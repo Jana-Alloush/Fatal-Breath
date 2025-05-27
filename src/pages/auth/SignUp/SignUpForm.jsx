299ca168b7f003e108d792d0301b5bf6c0e8794a
Jana-Alloush <janaalloush1234@gmail.com>
2025-05-27 16:11:23 +0300

    modified the color palette

--- src/pages/auth/SignUp/SignUpForm.jsx
+++ src/pages/auth/SignUp/SignUpForm.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import './SignUpForm.css';
+import React, { useState } from "react";
+import "./SignUpForm.css";
 import { FaUser, FaLock, FaEnvelope, FaPhone } from "react-icons/fa";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const SignUpForm = () => {
-  
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,10 +16,10 @@ const SignUpForm = () => {
 
   const handleLoginClick = (e) => {
     e.preventDefault();
-    navigate('/');
+    navigate("/");
   };
 
-/*const SignUpForm = ({ onSwitch }) => {
+  /*const SignUpForm = ({ onSwitch }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle sign up logic here
@@ -32,53 +31,56 @@ const SignUpForm = () => {
   };*/
 
   return (
-    <div className='wrapper'>
+    <div className="wrapper">
       <form onSubmit={handleSubmit}>
-        <h1>Create Account</h1>
-        
+        <h1 className="title">Create Account</h1>
+
         <div className="input-box">
-          <input type="text" placeholder='Full Name' required />
-          <FaUser className='icon' />
+          <input type="text" placeholder="Full Name" required />
+          <FaUser className="icon" />
         </div>
-        
+
         <div className="input-box">
-          <input 
-          type="email" 
-          placeholder='Email' 
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required 
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
           />
-          <FaEnvelope className='icon' />
+          <FaEnvelope className="icon" />
         </div>
-        
+
         <div className="input-box">
-          <input type="tel" placeholder='Phone Number' required />
-          <FaPhone className='icon' />
+          <input type="tel" placeholder="Phone Number" required />
+          <FaPhone className="icon" />
         </div>
-        
+
         <div className="input-box">
-          <input type="password" placeholder='Password' required />
-          <FaLock className='icon' />
+          <input type="password" placeholder="Password" required />
+          <FaLock className="icon" />
         </div>
-        
+
         <div className="input-box">
-          <input type="password" placeholder='Confirm Password' required />
-          <FaLock className='icon' />
+          <input type="password" placeholder="Confirm Password" required />
+          <FaLock className="icon" />
         </div>
 
         <div className="terms">
           <label>
-            <input type="checkbox" required /> 
-            I agree to the <a href="#">Terms & Conditions</a>
+            <input type="checkbox" required />I agree to the{" "}
+            <a href="#">Terms & Conditions</a>
           </label>
         </div>
-        
-        <button type="submit">Register</button>
-        
+
+        <button className="button" type="submit">Register</button>
+
         <div className="login-link">
-          <p>Already have an account? {' '}
-            <a href="#" onClick={handleLoginClick}>Login</a>
+          <p>
+            Already have an account?{" "}
+            <a href="#" onClick={handleLoginClick}>
+              Login
+            </a>
           </p>
         </div>
       </form>
